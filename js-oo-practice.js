9142c1d38e1ae7ea2ebf0f38afa1216114ec90ca
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-25 22:24:18 -0400

    using this

--- js-oo-practice.js
+++ js-oo-practice.js
@@ -4,6 +4,22 @@ var rabbit = {};
 
 rabbit.speak = function(line){
     console.log("The rabbit says '" + line + "'");
+    
+    
 }
 
 rabbit.speak("Im alive");
+
+
+/* this */
+
+function speak2(line){
+    console.log("The " + this.type + " rabbit says '" + line + "'");
+}
+
+var whiteRabbit = {type:"white", speak: speak2};
+var fatRabbit = {type: "fat", speak: speak2};
+
+whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
+
+fatRabbit.speak("I could sure use a carrot right now.");
