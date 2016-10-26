b84c26b422bdeb379a74326ad8425b9fc45d87da
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-25 22:38:05 -0400

    Prototypes

--- js-oo-practice.js
+++ js-oo-practice.js
@@ -23,3 +23,17 @@ var fatRabbit = {type: "fat", speak: speak2};
 whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
 
 fatRabbit.speak("I could sure use a carrot right now.");
+
+
+/* Prototypes:
+
+A prototype is another object that is used as a fallback source of properties. When an object gets a request for a property that it does not have, its prototype will be searched for the property, then the prototype’s prototype, and so on.
+*/
+
+var empty = {};
+console.log(empty.toString);
+console.log(empty.toString());
+
+console.log(Object.getPrototypeOf({}) == Object.prototype);
+console.log(Object.getPrototypeOf(isNaN) == Function.prototype);
+console.log(Object.getPrototypeOf([]) == Array.prototype);
